27aa18650cb2ba2499a7bd8623451db88bfac8cf
h82258652 <h82258652@hotmail.com>
2017-07-07 12:56:14 +0800

    update

---
+++ Homework5/Ruanmou.Advanced9.Homework5.AbstractFactory/AbstractFactory/FoodFactoryBase.cs
@@ -0,0 +1,17 @@
+﻿using Ruanmou.Advanced9.Homework5.Foods;
+
+namespace Ruanmou.Advanced9.Homework5.AbstractFactory
+{
+    public abstract class FoodFactoryBase
+    {
+        public abstract 菠萝油 Cook菠萝油();
+
+        public abstract 干炒牛河 Cook干炒牛河();
+
+        public abstract 罗宋汤 Cook罗宋汤();
+
+        public abstract 丝袜奶茶 Cook丝袜奶茶();
+
+        public abstract 猪扒包 Cook猪扒包();
+    }
+}
---
+++ Homework5/Ruanmou.Advanced9.Homework5.AbstractFactory/AbstractFactory/银龙茶餐厅.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using Ruanmou.Advanced9.Homework5.Foods;
+
+namespace Ruanmou.Advanced9.Homework5.AbstractFactory
+{
+    public class 银龙茶餐厅 : FoodFactoryBase
+    {
+        public override 菠萝油 Cook菠萝油()
+        {
+            throw new NotImplementedException();
+        }
+
+        public override 干炒牛河 Cook干炒牛河()
+        {
+            throw new NotImplementedException();
+        }
+
+        public override 罗宋汤 Cook罗宋汤()
+        {
+            throw new NotImplementedException();
+        }
+
+        public override 丝袜奶茶 Cook丝袜奶茶()
+        {
+            throw new NotImplementedException();
+        }
+
+        public override 猪扒包 Cook猪扒包()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
