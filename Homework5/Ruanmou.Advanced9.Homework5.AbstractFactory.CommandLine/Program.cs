72505c769d4a82b06af4b953c294d76d722e8a5d
h82258652 <h82258652@hotmail.com>
2017-07-07 12:50:07 +0800

    update

---
+++ Homework5/Ruanmou.Advanced9.Homework5.AbstractFactory.CommandLine/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Ruanmou.Advanced9.Homework5.AbstractFactory.CommandLine
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Homework5/Ruanmou.Advanced9.Homework5.SimpleFactory.CommandLine/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Ruanmou.Advanced9.Homework5.SimpleFactory.CommandLine
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("这里是简单工厂");
+
+            var foodSimpleFactory = new FoodSimpleFactory();
+            foodSimpleFactory.Cook("菠萝油").Show();
+            foodSimpleFactory.Cook("干炒牛河").Show();
+            foodSimpleFactory.Cook("罗宋汤").Show();
+            foodSimpleFactory.Cook("丝袜奶茶").Show();
+            foodSimpleFactory.Cook("猪扒包").Show();
+
+            Console.ReadKey();
+        }
+    }
+}
